ccb84004180f26570732a8a34d8b3d05720b189f
ratpik <pratikmandrekar@gmail.com>
2013-10-23 03:06:04 +0530

    Modified fabfile names

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,4 @@
-from fabric.api import env, local, require
-from fabric.context_managers import lcd
+from fabric.api import local
 import os
 
 def analyze():
@@ -7,10 +6,10 @@ def analyze():
     data_dir = os.path.join(pwd, 'data')
     files = os.listdir(data_dir)
     for f in files:
-        x = dict()
+        results = dict()
         for i in xrange(1,10):
             
-            x[i] = int(local("cat %s |awk '{print $NF}'|tr -sc [0-9] '\n'|sort -rn|grep ^%s|wc -l" %(os.path.join(data_dir,f), i), capture=True))
+            results[i] = int(local("cat %s |awk '{print $NF}'|tr -sc [0-9] '\n'|sort -rn|grep ^%s|wc -l" %(os.path.join(data_dir,f), i), capture=True))
         
-        print f
-        print x
+        print f #The state
+        print results #The count
